fcd526b17a675445f35d660eb48867d617ea71e0
thanakorn028 <thanakorn_job@hotmail.com>
2016-09-16 04:04:00 +0700

    add  and edit funcion

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,91 @@
 package cpe200;
 
+import java.text.DateFormat;
 
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+
+        this.firstOperand=operand;
     }
 
 
     public void setSecondOperand(Operand operand) {
+        this.secondOperand=operand;
     }
 
     public String add() {
-        return null;
+        double x = Double.parseDouble(firstOperand.getOperand())+Double.parseDouble(secondOperand.getOperand());
+        if(x == Math.floor(x)){
+            int t = (int)x;
+            return Integer.toString(t);
+        }
+        else{
+            return Double.toString(x);
+        }
     }
 
     public String subtract() {
-        return null;
+        double x = Double.parseDouble(firstOperand.getOperand())-Double.parseDouble(secondOperand.getOperand());
+        if(x == Math.floor(x)){
+            int t = (int)x;
+            return Integer.toString(t);
+        }
+        else{
+            x = x*100;
+            x = Math.ceil(x);
+            int y = (int)x;
+            x = (double)y / 100;
+            return Double.toString(x);
+        }
+
     }
 
     public String multiply() {
-        return null;
+        double x = Double.parseDouble(firstOperand.getOperand())*Double.parseDouble(secondOperand.getOperand());
+        if(x == Math.floor(x)){
+            int t = (int)x;
+            return Integer.toString(t);
+        }
+        else{
+            return Double.toString(x);
+        }
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        if(Double.parseDouble(secondOperand.getOperand()) != 0) {
+            double x = Double.parseDouble(firstOperand.getOperand()) / Double.parseDouble(secondOperand.getOperand());
+            if (x == Math.floor(x)) {
+                int t = (int) x;
+                return Integer.toString(t);
+            } else {
+                x = x*100000;
+                x = Math.floor(x);
+                int y = (int)x;
+                x = (double)y/100000;
+                return Double.toString(x);
+            }
+        }
+        else {
+            int x=1/0;
+            return "";
+        }
     }
 
     public String power() {
-        return null;
-    }
 
+        double x = Math.pow(Double.parseDouble(firstOperand.getOperand()) , Double.parseDouble(secondOperand.getOperand()));
+        if (x == Math.floor(x)) {
+            int t = (int) x;
+            return Integer.toString(t);
+        } else {
+            return Double.toString(x);
+        }
+    }
 }
--- lab4_1/src/cpe200/Operand.java
+++ lab4_1/src/cpe200/Operand.java
@@ -1,18 +1,23 @@
 package cpe200;
 
 public class Operand {
-    public String operand;
+    private String operand;
 
     public Operand(String operand) {
         this.operand = operand;
     }
 
     public Operand(int operand) {
+        this.operand=Integer.toString(operand);
 
     }
 
     public Operand(double operand) {
+        this.operand=Double.toString(operand);
 
     }
-
+    public String getOperand(){
+        return this.operand;
+    }
 }
+
