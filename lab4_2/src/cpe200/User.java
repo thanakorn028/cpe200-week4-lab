f79615b8ef66d5ce2aa632bce4fa762b0427f8f3
thanakorn028 <thanakorn_job@hotmail.com>
2016-09-16 04:05:10 +0700

    add funciton

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -3,26 +3,61 @@ package cpe200;
 /**
  * Created by pruet on 6/9/2559.
  */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class User {
     public String userName;
     public String password;
+    private static final String idREGEXUser = "([A-Za-z]+[a-zA-Z0-9]+)";
+
 
     public User() {
+
     }
 
     public boolean setUserName(String name) {
+        if(name.length()<8){
+            return false;
+        }
+
+        this.userName = name;
+
+        Pattern p = Pattern.compile(idREGEXUser);
+
+        Matcher m = p.matcher(this.userName);
+
+        if(m.matches()){
+            return true;
+        }
         return false;
     }
 
-    public boolean setPassword(String name) {
+    public boolean setPassword(String name)
+    {
+        if(name.length()<12){
+            return false;
+        }
+
+        this.password = name;
+
+        Pattern p1 = Pattern.compile("[a-z]");
+        Matcher m1 = p1.matcher(name);
+        Pattern p2 = Pattern.compile("[A-Z]");
+        Matcher m2 = p2.matcher(name);
+        Pattern p3 = Pattern.compile("[0-9]");
+        Matcher m3 = p3.matcher(name);
+        if(m1.find()&&m2.find()&&m3.find()){
+            return true;
+        }
         return false;
     }
 
     public String getUserName() {
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
--- lab4_2/src/cpe200/Users.java
+++ lab4_2/src/cpe200/Users.java
@@ -1,6 +1,7 @@
 package cpe200;
 
 import java.util.ArrayList;
+import java.util.jar.Attributes;
 
 /**
  * Created by pruet on 12/9/2559.
@@ -8,42 +9,73 @@ import java.util.ArrayList;
 public class Users {
     public ArrayList<User> userList;
 
+    public Users(){
+        userList=new ArrayList<>();
+    }
+
     public void addUser(User user)
     {
+        userList.add(user);
+
+
     }
 
     public void addUser(String userName, String password)
     {
+
     }
 
     public void deleteUser(User user)
+
     {
+        userList.remove(user);
+
 
     }
 
     public boolean exists(User user)
     {
+        for (int i = 0; i < userList.size(); i++) {
+            if (user == userList.get(i)) {
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean usernameExists(String username)
     {
+        for(int i = 0 ; i < userList.size();i++){
+            if(username == userList.get(i).getUserName()){
+                return true;
+            }
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public User getUserByUsername(String userName)
     {
+        for(int i = 0 ; i < userList.size();i++){
+            if(userName == userList.get(i).getUserName()){
+                return userList.get(i);
+            }
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public User[] getUserArray()
     {
-        return null;
+        User[] a = new User[userList.size()];
+        for(int i = 0 ; i < userList.size();i++){
+            a[i] =userList.get(i);
+        }
+        return a;
     }
+
 }
